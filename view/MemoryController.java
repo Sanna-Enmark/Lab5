35050f3da5815cf4b0eda39090e5294e38f28b6d
JesserD <47597441+JesserD@users.noreply.github.com>
2019-10-11 17:22:07 +0200

    Update MemoryController.java

--- view/MemoryController.java
+++ view/MemoryController.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package view;
+import model.MemoryLogic;
 
 /**
  *
- * @author senma
+ * @author Jesser
  */
 public class MemoryController {
+    
+    private final MemoryLogic model;
+    private final MemoryView view;
+    
+    public MemoryController(MemoryLogic model, MemoryView view) {
+        this.model = model;
+        this.view = view;
+    }
+    
+    
+    
+    
+    
     
 }
