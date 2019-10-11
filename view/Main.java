1c757279abad1942fa8cf891d8bc118d97b27955
JesserD <47597441+JesserD@users.noreply.github.com>
2019-10-11 18:06:18 +0200

    Main

---
+++ view/Main.java
@@ -0,0 +1,37 @@
+
+package view;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.MemoryLogic;
+
+/**
+ *
+ * @author Jesser
+ */
+/**
+ * Creates a window and ties model, view and controller together.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        // create model, view and controller, and tie them together
+        MemoryLogic model = new MemoryLogic();
+        MemoryView view = new MemoryView(model); // also creates the controller
+
+        // create the window, add the view, and show it
+        Scene scene = new Scene(view);
+        primaryStage.setTitle("Guess My Word");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
